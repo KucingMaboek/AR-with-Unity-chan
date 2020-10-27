7a741a1bfa9254035d14cb6188fb9239ff569239
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-10-27 12:00:31 +0700

    init commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public AnimationClip[] animations;
+    Animator anim;
+    private int currentExpression;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        // ensure if the face mask/layer weight is 1 
+        anim.SetLayerWeight(1, 1);
+    }
+
+    public void NextMotion()
+    {
+        anim.SetTrigger("NextTrigger");
+        RandomExpression();
+        // NextExpression();
+    }
+
+    public void NextExpression()
+    {
+        // If reached last expression, back to first expression
+        if (currentExpression < animations.Length - 1)
+        {
+            currentExpression++;
+        }
+        else
+        {
+            currentExpression = 0;
+        }
+
+        anim.SetLayerWeight(1, 1);
+        anim.CrossFade(animations[currentExpression].name, 0);
+    }
+
+    public void RandomExpression()
+    {
+        currentExpression = Random.Range(0, animations.Length - 1);
+        anim.CrossFade(animations[currentExpression].name, 0);
+    }
+}
---
+++ Assets/Scripts/Capture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Capture : MonoBehaviour
+{
+    [SerializeField] private AudioSource shutterSound;
+    private void Start()
+    {
+        // Request Storage Permission
+        Permission.RequestUserPermission(Permission.ExternalStorageRead);
+        Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+    }
+
+    public void TakeScreenshot()
+    {
+        StartCoroutine(ScreenshotProcessing());
+    }
+
+    private IEnumerator ScreenshotProcessing()
+    {
+        yield return null;
+
+        // Wait for buffering is complete
+        yield return new WaitForEndOfFrame();
+
+        // Preparing screenshot file name
+        string fileName = "Screenshot" + DateTime.Now.Hour + DateTime.Now.Minute +
+                          DateTime.Now.Second + ".png";
+
+        // Preparing a new location
+        string defaultLocation = Application.persistentDataPath + "/" + fileName;
+        string folderLocation = "/storage/emulated/0/DCIM/Camera/";
+        string targetLocation = folderLocation + fileName;
+
+        // Check if the new location exist
+        if (!Directory.Exists(folderLocation))
+        {
+            Directory.CreateDirectory(folderLocation);
+        }
+
+        // Take a Screenshot
+        ScreenCapture.CaptureScreenshot(fileName);
+        shutterSound.Play();
+
+        yield return new WaitForSeconds(1);
+
+        // Move Screenshot to new location
+        File.Move(defaultLocation, targetLocation);
+
+        // Refresh gallery to show new screenshot
+        AndroidJavaClass classPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject objActivity = classPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaClass classUri = new AndroidJavaClass("android.net.Uri");
+        AndroidJavaObject objIntent = new AndroidJavaObject("android.content.Intent",
+            "android.intent.action.MEDIA_MOUNTED",
+            classUri.CallStatic<AndroidJavaObject>("parse", "file://" + targetLocation));
+        objActivity.Call("sendBroadcast", objIntent);
+    }
+}
